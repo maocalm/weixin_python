db541efe54e83f48f21645e4d8aa992b48079c57
hanwenmao <maocalm135139@gmail.com>
2018-06-01 21:20:55 +0800

    flask ssss

--- flask_api/flask_study.py
+++ flask_api/flask_study.py
@@ -1,8 +1,7 @@
 import flask
-from flask import Flask, jsonify
+from flask import Flask, jsonify, url_for
 
-
-app = Flask(__name__)
+app = Flask(__name__)  #
 
 tasks = [
     {
@@ -19,9 +18,30 @@ tasks = [
     }
 ]
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])  # 装饰器
 def get_tasks():
     return jsonify({'tasks': tasks})
 
-if __name__ == '__main__':
+
+
+@app.route('/')
+def index():pass
+
+
+@app.route('/login')
+def login():pass
+
+
+@app.route('/user/<username>')
+def profile(username): pass
+
+
+if __name__ == '__main__':  # 确保该脚本只有在被python解释器执行的时候才被运行；
     app.run(debug=True)
+
+    # with app.test_request_context():
+    print(url_for('login'))
+    print(url_for('index'))
+    print(url_for('login' ,next='/'))
+    print(url_for('profile', username='John Doe'))
